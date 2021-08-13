39269296d4b4bfe7c3e132271f6386cb6f5ca702
David Ruffner <davidruffner924@gmail.com>
2021-08-12 20:21:00 -0400

    Initial Commit

---
+++ src/main/java/davidruffner/dryer_sensor/DryerSensorApplication.java
@@ -0,0 +1,13 @@
+package davidruffner.dryer_sensor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DryerSensorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DryerSensorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/davidruffner/dryer_sensor/DryerSensorApplicationTests.java
@@ -0,0 +1,13 @@
+package davidruffner.dryer_sensor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DryerSensorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
